6b579e6ec8a446f14fbc301dcaec62cb2b91fbe7
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-06-09 21:19:38 +0100

    mise en place données dans table users et dans la requete SQL de lancement

--- backend/SQL/data.sql
+++ backend/SQL/data.sql
@@ -1,4 +1,6 @@
 /*
+
+SOURCE D:/apprentissage/developpeur web/documents openclassrooms/livrables/P7/backend/SQL/data.sql
 ----------------------------------------------------------------------------------------------------------------
 -- Re-création de la base de données ----------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
@@ -30,11 +32,11 @@ CREATE TABLE IF NOT EXISTS `sn_users`
     `user_password` VARCHAR(255) NOT NULL, 
 	`user_registration` DATETIME DEFAULT NOW(),
 	`user_picture`VARCHAR(255) DEFAULT './uploads/profil/random-user.png',
-	`user_admin` BOOLEAN DEFAULT 0,
+	`user_admin` TINYINT(1) DEFAULT 0,
 
 
 	PRIMARY KEY(`id_user`)
-	P
+	
 )
 ENGINE = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
@@ -103,8 +105,31 @@ ENGINE = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 -- ajout user -----------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
 */
-INSERT INTO `sn_users`(`user_firstname`,`user_lastname`,`user_job`, `user_email`, `user_password`, `user_fullname`)
-VALUES('Daniel', 'Gonzalez','CEO', 'daniel@xyz.com', 'Azerty1',CONCAT(user_firstname,' ',user_lastname));
+INSERT INTO `sn_users`(`user_firstname`,`user_lastname`,`user_job`, `user_email`, `user_password`,`user_picture`, `user_fullname`, `user_admin`)
+VALUES
+('User', 'Admin','Modérateur', 'admin@groupomania.com', '$2b$10$rNqUBdTwQIkExDzyPZ61auJicwXOwe601zJNQKZ/rKHvkOeFIJSpO', './uploads/profil/random-user.png', CONCAT(user_firstname,' ',user_lastname), 1),
+('Pierre', 'Paulet','Directeur Général', 'pierre.paulet@groupomania.com', '$2b$10$/BPYtZhqfJM0Laa1/3abGeZHdvc3KNf1GFMX0i9gU7ZL376bF0eh6', './uploads/profil/2.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Fabien', 'Fouquet','Chef Comptable', 'fabien.fouquet@groupomania.com', '$2b$10$km6td7Pp/FZYmMa2pizcte7UkhbsVC4XFBXyxVHw.kv9rNy67QxR.', './uploads/profil/3.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Edouard', 'Foucault','Marketing', 'edouard.foucault@groupomania.com', '$2b$10$p/3D1uYUl7J//s7wq9J23ekcxtBAbxL12VpzExeQlh93H0mX59fDi', './uploads/profil/4.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Joseph', 'Bolduc','Chef des ventes', 'joseph.bolduc@groupomania.com', '$2b$10$G7Uql6QwacEkaeETtxy/uOH9NfssAdLf22WgP/0PMLfvis8XOC.0K', './uploads/profil/5.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Daniel', 'Gougeon','Commercial', 'daniel.gougeon@groupomania.com', '$2b$10$Hp1qobS2TGSMVLctohj.FO/fvLuGnMWDkVdVHzmKRAWE4xe4MADdK', './uploads/profil/6.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Clémence', 'Trottier','Directrice RH', 'clemence.trottier@groupomania.com', '$2b$10$r./r8Lkqt0bXFrcG3Fl31eTfDqlxPxOdXuP/hHG7gRnYBO8hS0dEe', './uploads/profil/7.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Alice', 'Durand','Marketing', 'alice.durand@groupomania.com', '$2b$10$XAwabJKOKH56nZwEAEVjAukuZ7YLTflO3FXtQOa4TLKqWkSopiLSK', './uploads/profil/8.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Julie', 'Bonnet','Commercial', 'julie.bonnet@groupomania.com', '$2b$10$pqXl1Yo7a8aNfpwk/rQSM.vWvOeuvgXiMkI3xRuow6Frb2mQZ1842', './uploads/profil/9.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Marine', 'Collin','Comptable', 'marine.collin@groupomania.com', '$2b$10$DBKzcdBUuPmsrw8jYJXGHuuAX/RPUno/GeUeYY9oKd4B9xWVD7ctO', './uploads/profil/10.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Stéphanie', 'Boivin','Chargée RH', 'stephanie.boivin@groupomania.com', '$2b$10$drwjrv3n51fRCmPnqbksxejc6QacYEZkgaNQ7dDI8CGcW8NROhIHG', './uploads/profil/11.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Alain', 'Brochu','Directeur Commercial', 'alain.brochu@groupomania.com', '$2b$10$teelIkoHpDgxFuIOIdiXpesHqCmrUnp.CRtJ/VrJnXPQK9iepR1Ou', './uploads/profil/12.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Margot', 'Pariseau','Assistante de direction', 'margot.pariseau@groupomania.com', '$2b$10$VzMYf9wAWJUdG8fC397zGeqCxM6uJGLhQ/iZiyj6z6KfGoA82PrvK', './uploads/profil/13.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Hugues', 'Maiseau','Contrôleur de gestion', 'hugues.maiseau@groupomania.com', '$2b$10$X8muWox.hEHP3oXWOqw/WOoKmeCYJj/kG34cBe0.Ktn55sbj0Kzz.', './uploads/profil/14.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Elodie', 'Lazure','Directrice Financière', 'elodie.lazure@groupomania.com', '$2b$10$LVwpaLu0CmKd.E1nda5BkO79IZ4aXakPtMvV8WA/Zx6PzX77FTDY2', './uploads/profil/15.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Enzo', 'Dupuis','Informaticien', 'enzo.dupuis@groupomania.com', '$2b$10$tErSHntxO9aKmdQ9wpRiHO1aVyY/mOV8H04y3yOpAkOhbRod357By', './uploads/profil/16.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Anne', 'Masson','Comptable', 'anne.masson@groupomania.com', '$2b$10$ZleyO4WiF5nVPZcs1uIkreU7HyOkJ5WgkFd.eE3sm68jiLmwjduz2', './uploads/profil/17.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Marcel', 'Blais','Chargé clientèle', 'marcel.blais@groupomania.com', '$2b$10$8DI2dkUyl/nbLSnsSTMD5.IXSHBdhTI9rymYvJJFLfMDFHNqilKiW', './uploads/profil/18.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Jeanne', 'Laforge','Trésorière', 'jeanne.laforge@groupomania.com', '$2b$10$AbYU2GZBnKZPhR7vQz4DheFY4/B3T4YDfsWLqkUU0o1nVR/vSxFqm', './uploads/profil/19.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Tristan', 'Marseau','Marketing', 'tristan.marseau@groupomania.com', '$2b$10$/mTjCBPPbMcAco/Uf3OWG.08MmeXZNINPhEcJJ84WhHkq.83Z6Uv6', './uploads/profil/20.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Charlotte', 'Laux','Responsable Région', 'charlotte.laux@groupomania.com', '$2b$10$WzwZ56H2MzTsCkk6JepIse2vbMZq.a2pSVGoMo0vCVMsXB4BB64kW', './uploads/profil/21.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Gabriel', 'Dodien','Chef de district', 'gabriel.dodien@groupomania.com', '$2b$10$eIRqlp4V1UfEQPKz0zqFLOmDSA6hmgFj8vPcbNcyYJRlJl0Z7nPUy', './uploads/profil/22.jpg ', CONCAT(user_firstname,' ',user_lastname), 0),
+('Justine', 'Clément','Assistante commerciale', 'justine.clement@groupomania.com', '$2b$10$62ZohfFAYOuVlTS5TQe6HOktvYUXJcEJdk0fi6QoIfnBOFlJzlymG', './uploads/profil/23.jpg ', CONCAT(user_firstname,' ',user_lastname), 0);
 
 /*
 ----------------------------------------------------------------------------------------------------------------
@@ -112,15 +137,15 @@ VALUES('Daniel', 'Gonzalez','CEO', 'daniel@xyz.com', 'Azerty1',CONCAT(user_first
 ----------------------------------------------------------------------------------------------------------------
 */
 
-INSERT INTO `sn_posts`(`post_title`,`post_content`, `post_image`, `post_author`)
-VALUES('2er post', 'Allez au travail!!!!', '', 2);
+INSERT INTO `sn_posts`(`post_content`, `post_image`, `post_author`)
+VALUES('Allez au travail!!!!', '', 2);
 
 /*
 ----------------------------------------------------------------------------------------------------------------
 -- modifie post -----------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------
 */
-UPDATE `sn_posts`SET `post_author` = 1 WHERE id_post = 1;
+/*UPDATE `sn_posts`SET `post_author` = 1 WHERE id_post = 1;*/
 
 /*
 ----------------------------------------------------------------------------------------------------------------
@@ -129,5 +154,5 @@ UPDATE `sn_posts`SET `post_author` = 1 WHERE id_post = 1;
 */
 
 INSERT INTO `sn_comments`(`comment_content`,`comment_author`, `comment_parent`, `comment_fullname`)
-VALUES('1er commentaire',1,1,'Daniel Gonzalez');
+VALUES('1er commentaire',2,1,'Pierre Paulet');
 
